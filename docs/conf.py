d8d14198ccd9de2620c6d4c7b33e2130b438d5ea
codists <codists@foxmail.com>
2021-05-24 20:02:48 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -28,9 +28,9 @@ author = 'codists'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    
 ]
-source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
