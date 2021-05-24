340b2e2cbd7d989eb74610f3cd3814bf636891fb
Hugh <609799548@qq.com>
2021-05-24 21:00:11 +0800

    update model relationship

--- back-end/blog/models.py
+++ back-end/blog/models.py
@@ -18,6 +18,8 @@ class User(db.Model):
     avatar = db.Column(db.String(100))
     _password = db.Column('password', db.String(128), nullable=False)
 
+    articles = db.relationship('Article', backref='user')
+
     @property
     def password(self):
         return self._password
@@ -28,6 +30,7 @@ class User(db.Model):
     
     def check_password(self, value):
         return check_password_hash(self._password, value)
+    
 
 
 class Article(db.Model):
@@ -39,4 +42,4 @@ class Article(db.Model):
     content = db.Column(db.Text, nullable=False)
     publish_time = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref='articles')
+
