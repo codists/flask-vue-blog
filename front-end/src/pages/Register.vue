294f1f7278cfec571f63fea003cfc1334b468468
codists <codists@foxmail.com>
2021-06-09 22:06:55 +0800

    首页添加背景图

---
+++ front-end/src/pages/Register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container-login">
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .container-login {
+        background-image: url('../../static/images/bg-login.jpg');
+    }
+</style>
