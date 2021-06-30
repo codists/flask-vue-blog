d5736eea42e1e62b501f078a9e95fe8bdff83ed7
codists <codists@foxmail.com>
2021-07-01 06:30:03 +0800

    首页左侧开发

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,27 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app" class="main-wrap">
     <Header></Header>
-    <div class="container">
-      内容区域
-    </div>
+    <div class="container">内容区域</div>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-
-
 // 导入组件，import隶属于 JavaScript 的范畴
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 // 注册组件
 export default {
-  components:{Header, Footer}
-}
+  components: { Header, Footer },
+};
 </script>
 
 <style>
-html, body, #app {
+html,
+body,
+#app {
   height: 100%;
 }
 
@@ -33,6 +31,7 @@ html, body, #app {
 .container {
   height: 80%;
 }
-
-
+.main-wrap {
+  min-width: 1200px;
+}
 </style>
---
+++ front-end/src/views/Register.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container-login">
+    <div class="main">
+      <div class="notes">
+        <h4>《EDVAC 报告书的第一份草案》</h4>
+        <p>
+          这种码和条形码是什么关系？1949
+          年，伍德兰和西尔弗基于摩尔斯电码发明了环状条形码，是条形码概念的最初提出者。此后，许多组织和个人为条形码的发展做出了贡献，其中最为突出的要数劳雷尔，他提出了斑马条纹状条形码。
+        </p>
+        <span>
+          <i></i>
+          6231
+        </span>
+      </div>
+      <div class="main-login">
+        <div class="login">
+          <span>帐号密码登录</span>
+          <span>手机免密登录</span>
+        </div>
+        <div class="register"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.container-login {
+  /* 可能存在的问题：图片加载比组件慢 */
+  background-image: url("../../static/images/bg-login.jpeg");
+  min-height: 600px;
+  background-size: cover;
+}
+.main {
+  min-height: 500px;
+  width: 790px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  margin-left: -395px;
+  margin-top: -250px;
+}
+.notes {
+  width: 340px;
+  height: 500px;
+  background: rgba(0, 0, 0, 0.4);
+  float: left;
+  /* 因为 div 里面有多个 内容，但是所有的都要保持边距，所以设置的是父div的 padding*/
+  padding: 40px;
+  color: #fff;
+  /* 定义如何计算总宽度和总高度， */
+  box-sizing: border-box;
+}
+.notes h4 {
+  /* 防止内容太长溢出 */
+  overflow: hidden;
+  /* 设置溢出的样式 */
+  text-overflow: ellipsis;
+  line-height: 34px;
+  height: 34px;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 24px;
+}
+.notes p {
+  line-height: 34px;
+  height: 270px;
+}
+.notes span {
+  position: absolute;
+  box-sizing: border-box;
+  min-width: 120px;
+  height: 40px;
+  padding: 0 10px;
+  color: #fc5531;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 24px 24px;
+  border: 1px solid #fc5531;
+  left: 40px;
+  bottom: 40px;
+  cursor: pointer;
+}
+.notes span i {
+  background-image: url("../../static/images/up_default.png");
+  display: inline-block;
+  width: 18px;
+  height: 18px;
+  background-size: cover;
+  /* position: relative; */
+  vertical-align: middle;
+  top: -3px;
+  margin-right: 8px;
+  animation-duration: 1s;
+}
+.main-login {
+  width: 450px;
+  height: 500px;
+  background-color: #fff;
+  float: right;
+  padding: 30px 0;
+}
+.main-login .login {
+  width: 50%;
+  margin: 40px auto 0;
+}
+</style>
